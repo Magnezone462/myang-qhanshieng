7c80f2561b38b134e2f74b7584010202ce29fac2
Kyt Logia <pact_fallacy0d@icloud.com>
2022-12-26 01:20:21 +0900

    themkra: nantoka zion mo dase masita

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,75 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { useKriemsakChangkho } from "./stores/kriemsak";
+import { jyongKriemsakkho } from "./stores/kriemsak";
 
-const drawer = ref(null);
-const kriemsakChangkho = useKriemsakChangkho();
-const krakshiwkqhruaSikrokXaumra = (xaumraco: string[]) => {
-  let ranranru = "";
-  for (const nope of xaumraco) {
-    ranranru = `${ranranru}、${nope}`;
-  }
-  return ranranru;
+type Drawer = boolean | null;
+
+const kriemsakkho = jyongKriemsakkho();
+
+const drawer = ref(<Drawer>null);
+
+const xuanThryuthei = () => {
+  drawer.value = !drawer.value;
 };
+const qhruaSikrokXaumra = (xaumraco: string[]) => xaumraco.join("、");
 </script>
 
 <template>
-  <v-app id="inspire">
-    <v-system-bar>
-      <h1>網絡本《漢字文聲義》</h1>
-      <v-spacer></v-spacer>
-    </v-system-bar>
+  <v-app>
+    <v-system-bar> </v-system-bar>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="xuanThryuthei()"></v-app-bar-nav-icon>
+      <v-toolbar-title>網絡本《漢字文聲義》</v-toolbar-title>
+    </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" bottom>
       <v-sheet color="grey-lighten-4" class="pa-4">
         <v-text-field
-          v-model="kriemsakChangkho.qhandziw"
+          v-model="kriemsakkho.qhandziwShyonip"
           label="漢字"
           placeholder="天地玄黄宇宙洪荒"
           clearable
         />
-        <v-btn block rounded @click="kriemsakChangkho.makeKriemsakKetkua()"
-          >檢索</v-btn
-        >
+        <v-btn block rounded @click="kriemsakkho.triwKriemsakKetkua()">
+          檢索
+        </v-btn>
       </v-sheet>
 
       <v-divider></v-divider>
 
       <v-list>
         <v-list-item
-          v-for="value in kriemsakChangkho.shyoKriemsakKetkua"
-          :key="value.pieutiwbyo"
+          v-for="baryu in kriemsakkho.twkKriemsakKetkua"
+          :key="baryu.pieutiwbyo"
+          @click="kriemsakkho.syendrakKriemsakKetkua(baryu)"
           link
         >
-          <v-list-item-content>
-            <v-list-item-title>{{ value.dziw }}</v-list-item-title>
-            {{ krakshiwkqhruaSikrokXaumra(value.sikrok_xaumra) }}
-          </v-list-item-content>
+          <v-list-item-title>{{ baryu.dziw }}</v-list-item-title>
+          {{ qhruaSikrokXaumra(baryu.sikrok_xaumra) }}
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-main> </v-main>
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            {{ kriemsakkho.twkSrioSyenKriemsakKetkua.dziw }}
+            <!-- {{
+              qhruaSikrokXaumra(
+                kriemsakkho.twkSrioSyenKriemsakKetkua.sikrok_xaumra
+              )
+            }} -->
+            <div
+              v-for="baryu in kriemsakkho.twkKriemsakKetkuaDziwqrim"
+              :key="baryu.pieutiwbyo"
+            >
+              {{ baryu.zyepheng }}
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
--- src/stores/kriemsak.ts
+++ src/stores/kriemsak.ts
@@ -1,8 +1,10 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import dziwxengSryokio from "../sryokio/dziwxeng.json";
+import dziwqrim from "../sryokio/dziwqrim.json";
+import dziwtyong from "../sryokio/dziwtyong.json";
+import dziwxeng from "../sryokio/dziwxeng.json";
 
-interface DziwxengSryokio {
+interface Dziwxeng {
   pieutiwbyo: string;
   dziw: string;
   dziw_QhSh: string;
@@ -13,41 +15,80 @@ interface DziwxengSryokio {
   tyoshiek: string;
 }
 
-export const useKriemsakChangkho = defineStore("kriemsak", () => {
-  const qhandziw = ref("");
-  const qhandziwco = ref(<string[]>[]);
-  const kriemsakKetkua = ref(<DziwxengSryokio[]>[]);
+interface Dziwqrim {
+  pieutiwbyo: string;
+  dziwdu: string;
+  dziwdu_QhSh: string;
+  zyepheng: string;
+  ChX_ziosryo_sishieng: string;
+  ChX_ziosryo_xrynmyuk: string;
+  ChX_ziosryo_sieuxryn: string;
+  diangko_xrynbu: string;
+  ChX_dzankryen_pyunlyi: string;
+  ChX_dzankryen: string[];
+  pyanchet_ChX: string;
+  pyanchet_ChX_tyoshiek: string;
+  pyanchet_XX: string;
+  pyanchet_XX_tyoshiek: string;
+  pyanchet_KX: string;
+  pyanchet_KX_tyoshiek: string;
+  pyanchet_DzX: string;
+  pyanchet_DzX_tyoshiek: string;
+  QhZQh_shienglyi: string;
+  QhZQh_xrynlyi: string;
+  QhZQh_deulyi: string;
+  giwtha_potyo: string;
+  ngrietyo: string;
+  thongqitmra: string;
+}
 
-  const shyoThytQhandziw = computed(() => qhandziw.value);
-  const shyoKriemsakKetkua = computed(() => kriemsakKetkua.value);
+export const jyongKriemsakkho = defineStore("kriemsak", () => {
+  const qhandziwShyonip = ref("");
+  const kriemsakKetkua = ref(<Dziwxeng[]>[]);
+  const srioSyenKriemsakKetkua = ref(<Dziwxeng>{});
+  const kriemsakKetkuaDziwqrim = ref(<Dziwqrim[]>[]);
 
-  const shyonipQhandziw = (cwixa: string) => {
-    qhandziwco.value = [];
-    for (const dziwbyo of cwixa) {
-      qhandziwco.value.push(dziwbyo);
-    }
-  };
+  const twkqhandziwShyonip = computed(() => qhandziwShyonip.value);
+  const twkKriemsakKetkua = computed(() => kriemsakKetkua.value);
+  const twkKriemsakKetkuaDziwqrim = computed(
+    () => kriemsakKetkuaDziwqrim.value
+  );
+  const twkSrioSyenKriemsakKetkua = computed(
+    () => srioSyenKriemsakKetkua.value
+  );
 
-  const makeKriemsakKetkua = () => {
+  const triwKriemsakKetkua = () => {
     kriemsakKetkua.value = [];
-    for (const dziwbyo of qhandziw.value) {
-      const dziwbyoKriemsakKetkua = dziwxengSryokio.filter((tuiziang) => {
+    for (const dziwbyo of qhandziwShyonip.value) {
+      const dziwbyoKriemsakKetkua = dziwxeng.filter((tuiziang) => {
         return tuiziang.dziw === dziwbyo;
       });
-      console.log(dziwbyoKriemsakKetkua);
       for (const yeah of dziwbyoKriemsakKetkua) {
         kriemsakKetkua.value.push(yeah);
       }
     }
   };
 
+  const syendrakKriemsakKetkua = (tuiziang: Dziwxeng) => {
+    srioSyenKriemsakKetkua.value = tuiziang;
+    kriemsakKetkuaDziwqrim.value = [];
+    const dziwqrimKriemsakKetkua = dziwqrim.filter((hoge) => {
+      return hoge.dziwdu === tuiziang.dziw;
+    });
+    for (const yeah of dziwqrimKriemsakKetkua) {
+      kriemsakKetkuaDziwqrim.value.push(yeah);
+    }
+  };
+
   return {
-    qhandziw,
-    qhandziwco,
+    qhandziwShyonip,
     kriemsakKetkua,
-    shyoThytQhandziw,
-    shyoKriemsakKetkua,
-    shyonipQhandziw,
-    makeKriemsakKetkua,
+    srioSyenKriemsakKetkua,
+    twkqhandziwShyonip,
+    twkKriemsakKetkua,
+    twkKriemsakKetkuaDziwqrim,
+    twkSrioSyenKriemsakKetkua,
+    triwKriemsakKetkua,
+    syendrakKriemsakKetkua,
   };
 });
