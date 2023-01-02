bc85eb18f0bdbff303c091928c889c9467608e70
Kyt Logia <pact_fallacy0d@icloud.com>
2023-01-02 16:04:03 +0900

    themkra: zihu no bukkumaku kino o tuika simasita

--- src/components/KriemsakPenlanXyutia.vue
+++ src/components/KriemsakPenlanXyutia.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { jyongDziwdeukho } from "@/stores/dziwdeu";
+import { jyongShiochiemkho } from "@/stores/shiochiem";
 import { jyongXeithongkho } from "@/stores/xeithong";
 
+const dziwdeukho = jyongDziwdeukho();
 const xeithongkho = jyongXeithongkho();
+const shiochiemkho = jyongShiochiemkho();
 </script>
 
 <template>
@@ -9,5 +13,15 @@ const xeithongkho = jyongXeithongkho();
     v-model="xeithongkho.kriemsakPenlanThryutheiXyutia"
     location="right"
   >
+    <v-list>
+      <v-list-item
+        v-for="dziwbyo in shiochiemkho.dziwbyoco"
+        :key="dziwbyo"
+        @click="dziwdeukho.triwSrioSyenDziwdu(dziwbyo)"
+        link
+      >
+        <v-list-item-title>{{ dziwbyo }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
   </v-navigation-drawer>
 </template>
---
+++ src/stores/shiochiem.ts
@@ -0,0 +1,27 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const jyongShiochiemkho = defineStore("shiochiem", () => {
+  // oyazi no bukkumaku hairetu desu
+  const dziwbyoco = ref(<string[]>[]);
+
+  // dziwbyoco kara oyazi o sakuzyo suru kansu desu
+  const drioDziwbyo = (grien: string) => {
+    dziwbyoco.value = dziwbyoco.value.filter((dziwbyo) => dziwbyo !== grien);
+  };
+
+  // dziwbyoco e oyazi o tuika suru kansu desu
+  const kraDziwbyo = (grien: string) => {
+    if (grien) dziwbyoco.value.push(grien);
+  };
+
+  // dziwbyoco ni oyazi ga aruka hante suru kansu desu
+  const xwmDziwbyo = (grien: string) => dziwbyoco.value.includes(grien);
+
+  // dziwbyoco no oyazi o kirikaeru kansu desu
+  const xuanDziwbyo = (grien: string) => {
+    xwmDziwbyo(grien) ? drioDziwbyo(grien) : kraDziwbyo(grien);
+  };
+
+  return { dziwbyoco, drioDziwbyo, kraDziwbyo, xwmDziwbyo, xuanDziwbyo };
+});
