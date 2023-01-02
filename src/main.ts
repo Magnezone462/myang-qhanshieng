d6841d3beeb739bc3819d454c122eec63b12cbec
Kyt Logia <pact_fallacy0d@icloud.com>
2023-01-02 16:15:20 +0900

    themkra: pinia no pasisuteddosuteto o insutoru simasita

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createPersistedState } from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
@@ -18,9 +19,12 @@ const vuetify = createVuetify({
   directives,
 });
 
+const pinia = createPinia();
+pinia.use(createPersistedState());
+
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(vuetify);
 
