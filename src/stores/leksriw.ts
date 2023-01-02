c1aa5422b81852c8f2fe0fefacf96daa4e8cc148
Kyt Logia <pact_fallacy0d@icloud.com>
2023-01-02 21:46:49 +0900

    themkra: rireki ni ezokuka o tuika simasita

--- src/stores/leksriw.ts
+++ src/stores/leksriw.ts
@@ -1,50 +1,54 @@
 import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 
-export const jyongLeksriwkho = defineStore("leksriw", () => {
-  // oyazi no rireki hairetu desu
-  const dziwbyoco = ref(<string[]>[]);
+export const jyongLeksriwkho = defineStore(
+  "leksriw",
+  () => {
+    // oyazi no rireki hairetu desu
+    const dziwbyoco = ref(<string[]>[]);
 
-  // dziwbyoco o gyakuzyun de kaesi masu
-  const dziwbyocoTentau = computed(() => dziwbyoco.value.reverse());
+    // dziwbyoco o gyakuzyun de kaesi masu
+    const dziwbyocoTentau = computed(() => dziwbyoco.value.reverse());
 
-  /**
-   * dziwbyoco o risetto suru kansu desu.
-   * @returns nanimo kaesimasen
-   */
-  const byukDziwbyoco = () => (dziwbyoco.value = []);
+    /**
+     * dziwbyoco o risetto suru kansu desu.
+     * @returns nanimo kaesimasen
+     */
+    const byukDziwbyoco = () => (dziwbyoco.value = []);
 
-  /**
-   * dziwbyoco ni oyazi ga aruka hante suru kansu desu.
-   * @param grien Sonzai o kakunin sitai oyazi desu.
-   * @returns Areba sin, nakereba gi o kaesimasu.
-   */
-  const diepyuXwmDziwbyo = (grien: string) => dziwbyoco.value.includes(grien);
+    /**
+     * dziwbyoco ni oyazi ga aruka hante suru kansu desu.
+     * @param grien Sonzai o kakunin sitai oyazi desu.
+     * @returns Areba sin, nakereba gi o kaesimasu.
+     */
+    const diepyuXwmDziwbyo = (grien: string) => dziwbyoco.value.includes(grien);
 
-  /**
-   * dziwbyoco kara oyazi o sakuzyo suru kansu desu.
-   * @param grien Aakuzyo suru oyazi desu.
-   */
-  const drioDziwbyo = (grien: string) => {
-    dziwbyoco.value = dziwbyoco.value.filter((dziwbyo) => dziwbyo !== grien);
-  };
+    /**
+     * dziwbyoco kara oyazi o sakuzyo suru kansu desu.
+     * @param grien Aakuzyo suru oyazi desu.
+     */
+    const drioDziwbyo = (grien: string) => {
+      dziwbyoco.value = dziwbyoco.value.filter((dziwbyo) => dziwbyo !== grien);
+    };
 
-  /**
-   * dziwbyoco e oyazi o tuika suru kansu desu.
-   * @param grien Tuika suru oyazi desu.
-   */
-  const kraDziwbyo = (grien: string) => {
-    if (grien && !diepyuXwmDziwbyo(grien)) {
-      dziwbyoco.value.push(grien);
-    }
-  };
+    /**
+     * dziwbyoco e oyazi o tuika suru kansu desu.
+     * @param grien Tuika suru oyazi desu.
+     */
+    const kraDziwbyo = (grien: string) => {
+      if (grien && !diepyuXwmDziwbyo(grien)) {
+        dziwbyoco.value.push(grien);
+      }
+    };
 
-  return {
-    dziwbyoco,
-    dziwbyocoTentau,
-    byukDziwbyoco,
-    diepyuXwmDziwbyo,
-    drioDziwbyo,
-    kraDziwbyo,
-  };
-});
+    return {
+      dziwbyoco,
+      dziwbyocoTentau,
+      byukDziwbyoco,
+      diepyuXwmDziwbyo,
+      drioDziwbyo,
+      kraDziwbyo,
+    };
+  },
+  { persist: true }
+);
