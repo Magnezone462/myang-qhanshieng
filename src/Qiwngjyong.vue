e0166dee2f1d0bc174e4556c2948a597d181b37a
Kyt Logia <pact_fallacy0d@icloud.com>
2023-06-18 23:30:38 +0900

    kra: ruto botan no barianto o sette simasita

--- src/Qiwngjyong.vue
+++ src/Qiwngjyong.vue
@@ -5,9 +5,21 @@ import { useTheme } from 'vuetify'
 import { Nguaikuan } from '@/cisren/muikio'
 import { jyongXeithongPundhikho } from '@/changkho/xeithongPundhi'
 import ShiettriwPenlan from '@/cogrien/ShiettriwPenlan.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 const tyodei = useTheme()
 const xeithongPundhikho = jyongXeithongPundhikho()
+const lojyu = useRoute()
+
+const qhanshiengnryuPryenthei = computed(() => {
+  if (lojyu.name === 'qhanshieng') return 'elevated'
+  else return 'tonal'
+})
+const qhanlaPryenthei = computed(() => {
+  if (lojyu.name === 'qhanla') return 'elevated'
+  else return 'tonal'
+})
 
 onMounted(() => {
   if (xeithongPundhikho.nguaikuan === Nguaikuan.dzidung) {
@@ -25,8 +37,18 @@ onMounted(() => {
     <ShiettriwPenlan temporary />
 
     <VSystemBar>
-      <VBtn @click="$router.push('/')"> 漢字文聲義</VBtn>
-      <VBtn @click="$router.push('/qhanla')"> 漢羅變换器</VBtn>
+      <VBtn
+        :variant="qhanshiengnryuPryenthei"
+        @click="$router.push({ name: 'qhanshieng' })"
+      >
+        漢字文聲義</VBtn
+      >
+      <VBtn
+        :variant="qhanlaPryenthei"
+        @click="$router.push({ name: 'qhanla' })"
+      >
+        漢羅變换器</VBtn
+      >
 
       <VSpacer />
 
