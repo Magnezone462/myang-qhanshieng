d7b17ae956c0a0425e33cac4bbcd2f808512d497
Kyt Logia <pact_fallacy0d@icloud.com>
2023-08-04 20:47:58 +0900

    sin: pakkezi o appudeto simasita

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ styles: { configFile: 'src/cisren/pryenliang.scss' } })],
+  plugins: [vue(), vueJsx(), vuetify({ styles: { configFile: 'src/cisren/pryenliang.scss' } })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
